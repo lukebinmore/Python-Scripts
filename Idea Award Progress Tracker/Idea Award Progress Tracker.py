9155ef544c8296430af933f9f956655d63e6c27e
Luke Binmore <lukebinmore@gmail.com>
2025-10-11 14:19:41 +0100

    Initial Commit

---
+++ Idea Award Progress Tracker/Idea Award Progress Tracker.py
@@ -0,0 +1,50 @@
+# region Library Imports
+
+# endregion
+
+# region Global Variables
+
+# endregion
+
+# region Classes
+
+
+class Student:
+    firstname = ""
+    lastname = ""
+    iName = ""
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.iName = self.lastname + self.firstname[0]
+
+
+# endregion
+
+# region Functions
+
+# endregion
+
+# region Main Code
+
+"""
+import student dataset
+decode and load dataset
+
+import analytics file
+decode and load analytics file
+
+import badges to check for
+
+match student dataset with analyics data
+
+remove students that have completed the badges provided
+
+Create student data output variable
+
+Export output data to file
+
+"""
+
+# endregion
